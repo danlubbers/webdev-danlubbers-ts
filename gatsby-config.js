b308ce7d74c8ed9e1368dc1d0ceda79d645de996
Dan Lubbers <lubbers.dan@gmail.com>
2020-10-12 12:30:24 -0400

    Refactored Technology component to loop over a JSON instead of static content and renamed logos for consistency throughout the app

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,22 @@ module.exports = {
   siteMetadata: {
     title: 'Dan Lubbers Web Development Site',
   },
-  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-sass'],
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-1733817-2',
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `json`,
+        path: `${__dirname}/src/JSON_Templates`,
+      },
+    },
+  ],
 };
